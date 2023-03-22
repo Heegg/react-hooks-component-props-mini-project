c23932370c441480b6e7204ec627d5d74345493e
Heeji Hyun <wlgmlgus@gmail.com>
2023-03-22 08:14:14 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -23,31 +23,3 @@ function App() {
 }
 export default App;
 
-
-
-// function App() {
-//   return (
-//     <>
-//       <Header name={blogData.name} />
-//       <About 
-//       image={blogData.image}
-//       about={blogData.about}/>
-//       <ArticleList
-//       posts={blogData.posts}
-//       />
-
-//         <div className="App">
-//           You're on your own from here! Follow the deliverables; test things out in
-//           the browser as you write your code; and good luck!
-//           <Article 
-//           title={blogData.posts.title}
-//           date={blogData.posts.date}
-//           preview={blogData.posts.preview}
-//           />
-//         </div>
-//     </>
-
-//   );
-// }
-
-// export default App;
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,19 +1,5 @@
 import React from 'react'
 
-// function Article({ id, title, date="January 1, 1970", preview }) {
-//   return (
-//     <>
-//       <article key={id}>
-//         <h3>{title}</h3>
-//         <small>{date}</small>
-//         {/* {date = null ? <small>date</small>  : "January 1, 1970"} */}
-//         {/* {date && null ? <small>{date}</small>  : "January 1, 1970"} */}
-//         <p>{preview}</p>
-//       </article>
-//     </>
-//   )
-// }
-
 function Article({ title, date="January 1, 1970", preview }) {
   return (
     <>
